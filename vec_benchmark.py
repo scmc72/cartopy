f3dd8f7f49dba3f3e650ffc9a477b56ad12756ef
daniel.attonbeckmann <beckmann@eld245.desktop.frd.metoffice.com>
2015-07-28 11:29:54 +0100

    changed bench marking to longer loop

--- vec_benchmark.py
+++ vec_benchmark.py
@@ -4,17 +4,17 @@ setup="""
 from cartopy.geodesic import Geodesic
 import numpy as np
 #random latlon points
-points = (np.random.rand(10,2)-0.5)
+points = (np.random.rand(1000,2)-0.5)
 points[:,0] *= 180
 points[:,1] *= 90
 
-endpoints = (np.random.rand(10,2)-0.5)
+endpoints = (np.random.rand(1000,2)-0.5)
 endpoints[:,0] *= 180
 endpoints[:,1] *= 90
 
-distance = np.random.rand(10)*100000
+distance = np.random.rand(1000)*100000
 
-azi = np.random.rand(10)*360
+azi = np.random.rand(1000)*360
 
 geod = Geodesic()
 
@@ -115,23 +115,23 @@ print "\nTESTING SPEEDS... \n\n"
 
 print "VECTORISED DIRECT"
 
-print timeit.timeit("v_d_benchmark(geod, points, azi, distance)", setup=setup, number = 5000)
+print timeit.timeit("v_d_benchmark(geod, points, azi, distance)", setup=setup, number = 1000)
 
 print "-----------------"
 
 print "BASIC LOOP DIRECT"
 
-print timeit.timeit("d_benchmark(geod, points, azi, distance)", setup=setup, number = 5000)
+print timeit.timeit("d_benchmark(geod, points, azi, distance)", setup=setup, number = 1000)
 
 print "############################"
 print "############################"
 
 print "VECTORISED INVERSE"
 
-print timeit.timeit("v_i_benchmark(geod, points, endpoints)", setup=setup, number = 5000)
+print timeit.timeit("v_i_benchmark(geod, points, endpoints)", setup=setup, number = 1000)
 
 print "-----------------"
 
 print "BASIC LOOP INVERSE"
 
-print timeit.timeit("i_benchmark(geod, points, endpoints)", setup=setup, number = 5000)
+print timeit.timeit("i_benchmark(geod, points, endpoints)", setup=setup, number = 1000)
